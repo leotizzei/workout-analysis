142a087539e4a3168c171750101edc31f742e40a
Leonardo Pondian Tizzei <leonardo.tizzei@gmail.com>
2020-12-28 23:45:36 -0300

    enhance plot

--- src/run_analysis.py
+++ src/run_analysis.py
@@ -1,7 +1,7 @@
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-import strava_client
+import json
 
 Z1 = (8, 7)
 Z2 = (7, 16)
@@ -10,18 +10,15 @@ Z4 = (5, 36)
 Z5 = (5, 7)
 Z6 = (4, 46)
 
-LABELS = ['Z1', 'Z2', 'Z3', 'Z4', 'Z5', 'Z6']
-COLORS = ['yellow', 'gray', 'blue', 'red', 'green', 'black']
+LABELS = ["avg_speed", 'Z1', 'Z2', 'Z3', 'Z4', 'Z5', 'Z6']
+COLORS = ['yellow', 'gray', 'blue', 'red', 'green']
+
 
 def convert_min_per_km_to_m_per_s(minutes: int, seconds: int):
     """
     """
     total_seconds = minutes * 60 + seconds
     return 1000 / total_seconds
-
-
-def convert_meters_per_second_to_min_per_km(mps: float):
-    pass
 
 
 def analyze_laps(laps: list, plot: bool = True):
@@ -31,10 +28,17 @@ def analyze_laps(laps: list, plot: bool = True):
         list of laps (average speed) of the last activity
 
     """
-    
+    y = [i.get("average_speed") for i in laps]
+    x = list()
+    dist = 0
+    for lap in laps:
+        dist += float(lap.get("distance"))
+        x.append(dist)
+
     if plot:
-        g = sns.scatterplot(x=range(0, len(laps)), y=laps)
-        for pace, label, color in zip([Z1,Z2,Z3,Z4,Z5,Z6], LABELS, COLORS):
+        sns.set_theme()
+        g = sns.lineplot(x=x, y=y, marker="o")
+        for pace, label, color in zip([Z1,Z2,Z3,Z4,Z5,Z6], LABELS[1:], COLORS):
             mps = convert_min_per_km_to_m_per_s(pace[0], pace[1])
             g.axhline(mps, label=label, c=color)
 
@@ -43,14 +47,12 @@ def analyze_laps(laps: list, plot: bool = True):
 
 
 def main():
-    laps_raw = strava_client.get_laps_of_last_run()
-    # print(laps_raw)
-    avg_speed = list()
-    for l in laps_raw:
-        # print(l)
-        # print('*' * 100)
-        avg_speed.append(float(l.get('average_speed')))
-    analyze_laps(laps=avg_speed, plot=True)
+    # laps_raw = strava_client.get_laps_of_last_run()
+    # with open("laps.txt", "w+") as f:
+    #     f.write(str(laps_raw))
+    with open("laps.txt", "r") as f:
+        laps = json.load(f)
+    analyze_laps(laps=laps, plot=True)
 
 
 if __name__ == '__main__':
